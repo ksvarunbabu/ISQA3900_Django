2ac67aaf744a347474b68719ab5e9017ce35bfee
Varun KS <ksvarun@gmail.com>
2024-01-14 17:39:43 -0600

    migrate

--- VK_Tea_Shop/VK_Tea_Shop/settings.py
+++ VK_Tea_Shop/VK_Tea_Shop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MyTea.apps.MyteaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
