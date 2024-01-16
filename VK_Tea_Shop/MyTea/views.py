3f52dbbaa10a458d54351b8ae3b0a3b2707e8400
Varun KS <ksvarun@gmail.com>
2024-01-15 20:38:19 -0600

    skeleton for html and views

--- VK_Tea_Shop/MyTea/views.py
+++ VK_Tea_Shop/MyTea/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
   return HttpResponse("Welcome to VK's Tea Shop. Enjoy your Drinks")
+
+class HomePageView(TemplateView):
+  template_name = 'home.html'
+
+class WelcomePage(TemplateView):
+  template_name = "welcome.html"
