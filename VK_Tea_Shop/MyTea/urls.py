b4d1491d21e5cda50bfebf2f45502627e1553089
Varun KS <ksvarun@gmail.com>
2024-01-28 18:51:02 -0600

    a few html modifications

--- VK_Tea_Shop/MyTea/urls.py
+++ VK_Tea_Shop/MyTea/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import HomePageView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('home/',HomePageView.as_view(), name='home'),
+    path("test/", views.index, name="index"),
+    path('',HomePageView.as_view(), name='home'),
     path('welcome/',views.WelcomePage.as_view(), name='welcome')
 ]
